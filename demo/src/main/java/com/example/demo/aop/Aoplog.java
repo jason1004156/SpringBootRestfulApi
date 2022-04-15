389a8041a4a0c6ea34d4fa9ca05ae513e3f19cdc
jason1004156 <jason1004156@gmail.com>
2022-04-15 15:19:13 +0800

    Fommat

--- demo/src/main/java/com/example/demo/aop/Aoplog.java
+++ demo/src/main/java/com/example/demo/aop/Aoplog.java
@@ -23,7 +23,7 @@ import org.springframework.web.util.WebUtils;
 public class Aoplog {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	private static final String AOPWEBLOG = "aopWebLog()";
 
 	// 執行緒局部變數，用於解決多執行緒衝突
@@ -39,7 +39,7 @@ public class Aoplog {
 	public void doBefore(JoinPoint j) {
 		startTime.set(System.currentTimeMillis());
 		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-		if(attributes == null) {
+		if (attributes == null) {
 			return;
 		}
 		HttpServletRequest r = attributes.getRequest();
@@ -48,8 +48,8 @@ public class Aoplog {
 		log.info("IP位址:" + r.getRemoteAddr());
 		log.info("類別的方法:" + j.getSignature().getDeclaringTypeName() + "." + j.getSignature().getName());
 		log.info("參數:" + r.getQueryString());
-        log.info("request body:" + getPayload(r));
-		
+		log.info("request body:" + getPayload(r));
+
 	}
 
 	@AfterReturning(pointcut = AOPWEBLOG, returning = ("retObject"))
@@ -57,25 +57,25 @@ public class Aoplog {
 		log.info("回應值:" + retObject);
 		log.info("費時:" + (System.currentTimeMillis() - startTime.get()));
 	}
-	
+
 	@AfterThrowing(pointcut = AOPWEBLOG, throwing = "e")
-	public void doAfterThorowing(JoinPoint j,Exception e) {
+	public void doAfterThorowing(JoinPoint j, Exception e) {
 		log.error("執行例外", e);
 	}
-	
-	 private String getPayload(HttpServletRequest request) {
-	        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
-	        if (wrapper != null) {
-	            byte[] buf = wrapper.getContentAsByteArray();
-	            if (buf.length > 0) {
-	                try {
-	                    int length = Math.min(buf.length, 1024);// 最多只印出1024長度
-	                    return new String(buf, 0, length, wrapper.getCharacterEncoding());
-	                } catch (UnsupportedEncodingException ex) {
-	                    return "[unknown]";
-	                }
-	            }
-	        }
-	        return "";
-	    }
+
+	private String getPayload(HttpServletRequest request) {
+		ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+		if (wrapper != null) {
+			byte[] buf = wrapper.getContentAsByteArray();
+			if (buf.length > 0) {
+				try {
+					int length = Math.min(buf.length, 1024);// 最多只印出1024長度
+					return new String(buf, 0, length, wrapper.getCharacterEncoding());
+				} catch (UnsupportedEncodingException ex) {
+					return "[unknown]";
+				}
+			}
+		}
+		return "";
+	}
 }
